a7163355fcb979441aace29dd7ea8efd4d251c29
SUZUKI <ks3510024@email.com>
2020-08-22 02:50:05 +0000

    7.1.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  # 「remember_token」という仮想の属性を作成します。
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   
   validates :name, presence: true, length: { maximum: 50 }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
@@ -24,4 +27,10 @@ class User < ApplicationRecord
   def User.new_token
     SecureRandom.urlsafe_base64
   end
+
+  # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end 
 end
