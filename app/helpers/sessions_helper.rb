98e5dbf935b84a4cf558afaf8491fec668129e24
SUZUKI <ks3510024@email.com>
2020-08-22 11:17:07 +0000

    8.2.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,7 +39,12 @@ module SessionsHelper
     end
   end 
 
-# 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返します。
+  def current_user?(user)
+    user == current_user
+  end 
+
+  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
   def logged_in?
     !current_user.nil?
   end
